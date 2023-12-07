db9ef77482d4892b1e9a9c9a55f161fa8464a4ab
Muhammad Ahmad <ahmad481988@gmail.com>
2023-12-07 10:38:16 +0500

    commit

--- cicd_testing/hello-world/index.mjs
+++ cicd_testing/hello-world/index.mjs
@@ -2,7 +2,7 @@ import log4js from "log4js";
 
 export const handler = async (event, context) => {
     try {
-        const response = "Hello World! from Ahmad";
+        const response = "Hello World! from Ahmad GCU";
         logger.info(`${response}`);
         return response;
     } catch (err) {
