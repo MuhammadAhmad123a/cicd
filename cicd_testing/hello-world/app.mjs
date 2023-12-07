5f09c35c205cd78d4b219beef71874774b824f9e
Muhammad Ahmad <ahmad481988@gmail.com>
2023-12-07 10:02:30 +0500

    commit

--- cicd_testing/hello-world/app.mjs
+++ cicd_testing/hello-world/app.mjs
@@ -2,7 +2,7 @@ import log4js from "log4js";
 
 export const lambdaHandler = async (event, context) => {
     try {
-        const response = "Hello World!";
+        const response = "Hello World! from OB";
         logger.info(`${response}`);
         return response;
     } catch (err) {
