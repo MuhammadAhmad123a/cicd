365a164e095caa1da051cb85d9fea7b606783e92
Muhammad Ahmad <ahmad481988@gmail.com>
2023-12-07 21:39:38 +0500

    commit

--- hello-world/index.mjs
+++ hello-world/index.mjs
@@ -2,7 +2,7 @@ import log4js from "log4js";
 
 export const handler = async (event, context) => {
     try {
-        const response = "Hello World! from Ahmad GCU";
+        const response = "Hello World from Muhammad Ahmad!";
         logger.info(`${response}`);
         return response;
     } catch (err) {
