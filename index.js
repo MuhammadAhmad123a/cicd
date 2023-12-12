05bdd1194c84703ae961057b59c9e18aea5d3a02
Muhammad Ahmad <ahmad481988@gmail.com>
2023-12-12 10:06:39 +0500

    COMMIT

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import log4js from "log4js";
 export const handler = async (event, context) => {
     try {
         const response = "Hello World from Muhammad Ahmad GCU!";
-        logger.info(`${response}`);
+        console.log(response);
         return response;
     } catch (err) {
-        logger.error(`${err}`);
+        console.log(err);
         return err;
     }
 };
